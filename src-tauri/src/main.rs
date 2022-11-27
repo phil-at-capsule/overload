96489eaca3a0e66dc79f1ed77b2adae97d6b214d
Phil Marsh <phil@capsule-ai.co.uk>
2022-11-27 10:47:26 +0000

    Start json deser

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,11 @@
   windows_subsystem = "windows"
 )]
 
+use serde_json;
+use std::fs;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
 enum Muscle {
   Cardio,
   Neck,
@@ -24,6 +29,7 @@ enum Muscle {
   Calfs
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 struct ExerciseType {
   name: String,
   machine: bool,
@@ -31,41 +37,60 @@ struct ExerciseType {
   minor: Vec<Muscle>,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 struct TimeDuration {
   time: u8,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 struct RepRange {
   start: u8,
   end: u8,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 struct RepDuration {
   sets: u8,
   rep_range: RepRange,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 enum Duration {
   Time(TimeDuration),
-  Rep(RepDuration),
+  Sets(RepDuration),
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 struct Exercise {
   exercise: ExerciseType,
   duration: Duration,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 struct Session {
   location: String,
   exercises: Vec<Exercise>
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 struct Period {
   sessions: Vec<Session>
 }
 
+#[tauri::command]
+fn print() -> String {
+  "print".to_string()
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+  let path = "../db/periods.json";
+  let data = fs::read_to_string(path).expect("Unable to read file");
+  let periods: Vec<Period> = serde_json::from_str(&data).expect("blah");
+  println!("{periods:?}")
+  // tauri::Builder::default()
+  //     .invoke_handler(tauri::generate_handler![
+  //       print
+  //     ])
+  //   .run(tauri::generate_context!())
+  //   .expect("error while running tauri application");
 }
